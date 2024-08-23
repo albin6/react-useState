5583b4204354d915059556eef408ace57a802a09
Albin Aji <albinpariyarathu@gmail.com>
2024-08-23 09:12:15 +0530

    useState stage 5

--- UnwantedUsage.js
+++ UnwantedUsage.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from 'react'
 const PRICE_PER_ITEM = 5
 function UnwantedUsage() {
     const [quantity, setQUantity] = useState(0)
-    const [totalPrice, setTotalPrice] = useState(0)
+    // const [totalPrice, setTotalPrice] = useState(0) // dont need to use a new state variable
+    const totalPrice = quantity * PRICE_PER_ITEM
+    // it will automatically calculated the total price in each render cycle
 
-
-    useEffect(() => {
-        setTotalPrice(quantity * PRICE_PER_ITEM)
-    }, [quantity])
+    // useEffect(() => {
+    //     setTotalPrice(quantity * PRICE_PER_ITEM)
+    // }, [quantity])
     
   return (
     <div>
