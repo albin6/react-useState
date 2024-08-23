78138d19f655b0768d73ff3438d3cc5627a736b9
Albin Aji <albinpariyarathu@gmail.com>
2024-08-23 06:40:00 +0530

    useState stage 2 complete

--- Conditional.js
+++ Conditional.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // here we can render the content according to the project
 const Conditional = () => {
     let data = false; // assume that the data is passed as props
@@ -7,12 +8,25 @@ const Conditional = () => {
                 <h1>No data found</h1>
             </div>
         )
-    } // this is known as conditional rendering
+    }
+    // any statement after the above if will not get executed if no data is passed
+    const [status, setStatus] = useState(0)
     return (
         <div>
-            <h1>Waiting for data</h1>
+            <h1>data recieved</h1>
         </div>
     )
+    // instead of having two return we do it in one
+    return (<section>{
+        (!data) ? (
+            <div>
+                <h1>No data found</h1>
+            </div>
+        ) : (<div>
+            <h1>data recieved</h1>
+        </div>)
+    }</section>)
+    // this is known as conditional rendering
 }
 
 export default Conditional;
